9323177b703325ed66e81e4c1f1ed74e691e1b76
mathventist <mathventist@gmail.com>
2021-12-21 22:16:04 -0700

    Commit for v0.1.0

--- compareWord2Vec.go
+++ compareWord2Vec.go
@@ -4,14 +4,16 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"sync"
+	"sort"
 
 	"code.sajari.com/word2vec"
 	"github.com/mathventist/duplicates"
+	"github.com/mathventist/heatmap"
 	"github.com/schollz/progressbar/v3"
 )
 
 var model *word2vec.Model
+var cache word2vec.Coser
 
 type matchGroup struct {
 	A        string
@@ -23,19 +25,23 @@ type matchGroup struct {
 func w2v() {
 	var removeStops bool
 	var score float64
+	var heatMapFileName string
 
 	flags := flag.NewFlagSet("w2v", flag.ExitOnError)
 	flags.BoolVar(&removeStops, "r", true, "remove stop words from text")
 	flags.BoolVar(&removeStops, "removeStops", true, "remove stop words from text")
 	flags.Float64Var(&score, "s", float64(0.5), "minimum score for which matches will be reported")
 	flags.Float64Var(&score, "score", float64(0.5), "minimum score for which matches will be reported")
+	flags.StringVar(&heatMapFileName, "heatmap", "", "filename of the generatid heatmap")
+	flags.StringVar(&heatMapFileName, "h", "", "filename of the generatid heatmap")
 
 	flags.Usage = func() {
-		usageText := `usage: dups w2v [ -h | --help ] [ -r | --removeStops ] [ -s <score> | --score <score>] <Word2Vec model file> <file A> <file B>
+		usageText := `usage: dups w2v [ -h | --help ] [ -r | --removeStops ] [ -s <score> | --score <score>] [ -h <filename> | --heatmap <filename> ] <Word2Vec model file> <file A> <file B>
 
-        -h, --help            print the help message
-        -r, --removeStops     remove English stop words from the text
-        -s, --score <score>   only return matches greater or equal to this value between 0 and 1`
+        -h, --help                 print the help message
+        -r, --removeStops          remove English stop words from the text
+        -s, --score <score>        only return matches greater or equal to this value between 0 and 1; ignored if -h is set
+        -h, --heatmap <filename>   generate a heatmap of the scores with the given filename; -s is ignored when this is set`
 
 		fmt.Println(usageText)
 	}
@@ -67,6 +73,8 @@ func w2v() {
 	defer f.Close()
 
 	model, err = word2vec.FromReader(f)
+	cache = word2vec.NewCache(model)
+
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error loading word2vec model: %v\n", err)
 		os.Exit(1)
@@ -74,6 +82,32 @@ func w2v() {
 
 	r := w2vCompare(a, b, fileName1, fileName2, removeStops)
 
+	if heatMapFileName != "" {
+		generateHeatMap(r, heatMapFileName)
+	} else {
+		printResults(r, score)
+	}
+}
+
+func generateHeatMap(r [][]matchedStrings, heatMapFileName string) {
+	data := make([][]float32, len(r))
+	for j := range data {
+		data[j] = make([]float32, len(r[0]))
+	}
+
+	for i, n := range r {
+		for j, m := range n {
+			data[i][j] = float32(m.score)
+		}
+	}
+
+	if err := heatmap.DrawHeatMap(data, 25, heatMapFileName); err != nil {
+		fmt.Fprintf(os.Stderr, "error generating heatmap: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func printResults(r [][]matchedStrings, score float64) {
 	var results []result
 
 	for i, n := range r {
@@ -89,10 +123,17 @@ func w2v() {
 		}
 	}
 
+	// sort highest matches first
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].match.score > results[j].match.score
+	})
+
 	fmt.Printf("%v\n", results)
 }
 
-func w2vCompare(a, b []string, fileName1, fileName2 string, removeStops bool) [][]matchedStrings {
+// TODO: fix this concurrent version, or delete.
+/*
+func oldw2vCompare(a, b []string, fileName1, fileName2 string, removeStops bool) [][]matchedStrings {
 	ca := preprocess(a, fileName1, removeStops)
 	cb := preprocess(b, fileName2, removeStops)
 
@@ -126,6 +167,39 @@ func w2vCompare(a, b []string, fileName1, fileName2 string, removeStops bool) []
 
 				bar.Add(1)
 			}(aa, bb, i, j)
+		}
+	}
+
+	return results.matches
+}
+*/
+
+func w2vCompare(a, b []string, fileName1, fileName2 string, removeStops bool) [][]matchedStrings {
+	ca := preprocess(a, fileName1, removeStops)
+	cb := preprocess(b, fileName2, removeStops)
+
+	la, lb := <-ca, <-cb
+
+	results := newIndexedStrings(len(a), len(b))
+
+	bar := progressbar.Default(int64(len(a)*len(b)), "comparing files...")
+
+	for i, aa := range la {
+		if len(aa.processed) == 0 {
+			bar.Add(len(lb))
+			continue
+		}
+
+		for j, bb := range lb {
+			if len(bb.processed) == 0 {
+				bar.Add(1)
+				continue
+			}
+
+			normalizedScore, _ := duplicates.CompareWord2Vec(aa.processed, bb.processed, model, cache)
+			results.Add(i, j, aa.original, bb.original, float64(normalizedScore))
+
+			bar.Add(1)
 		}
 	}
 
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	code.sajari.com/word2vec v1.0.0
 	github.com/golang-collections/collections v0.0.0-20130729185459-604e922904d3
-	github.com/mathventist/duplicates v0.3.0
-	github.com/schollz/progressbar/v3 v3.8.2
+	github.com/mathventist/duplicates v0.4.0
+	github.com/mathventist/heatmap v1.0.0
+	github.com/schollz/progressbar/v3 v3.8.5
 )
